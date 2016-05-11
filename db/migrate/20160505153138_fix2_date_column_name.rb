795c419415d384d64701c867f787d5363829fe67
Sergey Shchepetkov <sgh0st9@gmail.com>
2016-05-11 18:49:04 +0300

    column type fix

--- db/migrate/20160505153138_fix2_date_column_name.rb
+++ db/migrate/20160505153138_fix2_date_column_name.rb
@@ -1,7 +1,10 @@
 class Fix2DateColumnName < ActiveRecord::Migration
   def change
     rename_column :events, :start_date, :date
-    change_column :events, :date, :datetime
     remove_column :events, :start_time, :datetime
+    execute <<-SQL
+      ALTER TABLE "events" ALTER COLUMN "date" TYPE timestamp
+      USING ('2000-1-1'::date + date)
+    SQL
   end
 end
